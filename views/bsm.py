0c482002ce2f40ffc30c530c0c2b6c4d37d50db3
Nickyrayy <Nickyray.Github@gmail.com>
2025-07-12 22:28:45 +1000

    created basic sidebar with required parametres + more for a more robust bsm model implmentation down the road

--- views/bsm.py
+++ views/bsm.py
@@ -12,3 +12,89 @@ st.set_page_config(
 
 )
 
+st.write("BSM")
+
+with st.sidebar:
+    st.subheader("Required Variables")
+
+    S = st.number_input(
+        "Current Asset Price $", 
+        min_value=0.0, 
+        value=100.0, 
+        step=5.0, 
+        format="%0.2f",
+        key="S",
+    )
+
+    K = st.number_input(
+        "Current strike Price $", 
+        min_value=0.0, 
+        value=100.0, 
+        step=5.0, 
+        format="%0.2f",
+        key="K",
+    )
+
+    T = st.number_input(
+        "Time to expiration (Annualised)", 
+        min_value=0.0, 
+        value=1.0, 
+        step=0.1, 
+        format="%0.2f",
+        key="T",
+    )
+
+    r = st.number_input(
+        "Risk-Free Interest Rate % (Annualised)", 
+        min_value=0.0, 
+        value=5.0, 
+        step=0.05, 
+        format="%0.2f",
+        key="r",
+    )
+
+    v= st.number_input(
+        "Volatility %", 
+        min_value=0.00, 
+        value=20.0, 
+        step=0.01, 
+        format="%0.2f",
+        key="v",
+    )
+        
+    st.divider()
+
+    st.subheader("Optional Variables")
+
+    q = st.number_input(
+        "Expected Dividend Yield %", 
+        min_value=0.0, 
+        value=0.0, 
+        step=0.05, 
+        format="%0.2f",
+        key="q",
+    )
+
+    st.write("Transaction costs $")
+
+    k = st.number_input(
+        "Round trip transaction cost %",
+        min_value=0.0,
+        value=0.0,
+        step=0.05,
+        format="%0.2f",
+        key="k",
+        help="All expenses for buying and selling the option as a percentage"
+    )
+
+    dt = st.number_input(
+    "Δ Time (trading days)", 
+    min_value=0.0, 
+    value=0.0, 
+    step=1.0, 
+    format="%0.2f",
+    key="tc",
+    help="The time between hedging adjustment's (days)",
+    )
+
+    st.divider()
